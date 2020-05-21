78738723393a4750b90b4aec3d0e6aa7b3024deb
user <irchello@gmail.com>
2020-05-21 09:25:44 -1000

    Updates

---
+++ precache-manifest.d36bf3a904a5a579b75bb3bc6e0cce0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508dc3227d36e080565a32d9283e0dfe",
+    "url": "/stock-tweets/index.html"
+  },
+  {
+    "revision": "26c9aad7e72b06863b52",
+    "url": "/stock-tweets/static/css/main.b48c5502.chunk.css"
+  },
+  {
+    "revision": "ed2d5efff61422493a9f",
+    "url": "/stock-tweets/static/js/2.a45fe27e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-tweets/static/js/2.a45fe27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c9aad7e72b06863b52",
+    "url": "/stock-tweets/static/js/main.6ce40f87.chunk.js"
+  },
+  {
+    "revision": "a76b4fad3f5ee8a572bd",
+    "url": "/stock-tweets/static/js/runtime-main.813dbf39.js"
+  }
+]);
